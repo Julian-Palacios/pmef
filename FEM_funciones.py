deef7029d50d1cc15e45971b5559b12cc8838b7e
Julian-Palacios <jpalaciose@uni.pe>
2022-02-11 17:56:21 -0500

    test VSC

--- FEM_funciones.py
+++ FEM_funciones.py
@@ -1,11 +1,11 @@
 # Código creado en el año 2020 por Julian Palacios (jpalaciose@uni.pe)
 # Con la colaboración de Jorge Lulimachi (jlulimachis@uni.pe)
 # Codificado para la clase FICP-C811 en la UNI, Lima, Perú
-##
+#
 # Este código se basó en las rutinas de matlab FEMCode
 # realizado inicialmente por Garth N .Wells (2005)
 # para la clase CT5123 en TU Delft, Países Bajos
-##
+#
 
 import numpy as np
 from numpy.linalg import det, inv
